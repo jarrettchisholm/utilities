4accdd98a70c2daedb826ca32e670088b91c8268
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2013-11-12 12:57:12 -0500

    - added properties class

---
+++ Properties.cpp
@@ -0,0 +1,100 @@
+/*
+ * Properties.cpp
+ *
+ *  Created on: 2013-05-06
+ *	  Author: jarrett
+ */
+
+#include <iostream>
+#include <string>
+#include <set>
+#include <sstream>
+#include <exception>
+#include <fstream>
+
+#include <iomanip>
+#include <algorithm>
+#include <cctype>
+
+#include "Properties.h"
+
+namespace utilities {
+	
+Properties::Properties(std::string filename) : filename_(filename)
+{
+	initialize();
+}
+
+Properties::~Properties()
+{
+}
+
+void Properties::initialize()
+{	
+	std::ifstream config( filename_.c_str() );
+	if(!config)
+	{
+		// TODO: proper error handling
+		std::cerr<<"error"<<std::endl;
+		return;
+	}
+
+	//parameters
+	std::set<std::string> options;
+	options.insert("*");
+	
+	try
+	{	  
+		for (pod::config_file_iterator i(config, options), e ; i != e; ++i)
+		{
+			std::cout << i->string_key <<" "<<i->value[0] << std::endl;
+			parameters_[i->string_key] = i->value[0];
+		}
+		std::cout << parameters_["StatLogServer.Path"] << std::endl;
+	}
+	catch(std::exception& e)	
+	{
+		std::cerr<<"Exception: "<<e.what()<<std::endl;
+	}
+}
+
+bool Properties::toBool(std::string str)
+{
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    std::istringstream is(str);
+    bool b;
+    is >> std::boolalpha >> b;
+    return b;
+}
+
+std::string Properties::getStringValue(std::string name)
+{
+	return parameters_[name];
+}
+
+int Properties::getIntValue(std::string name)
+{
+	return std::stoi( parameters_[name] );
+}
+
+long Properties::getLongValue(std::string name)
+{
+	return std::stol( parameters_[name] );
+}
+
+float Properties::getFloatValue(std::string name)
+{
+	return std::stof( parameters_[name] );
+}
+
+double Properties::getDoubleValue(std::string name)
+{
+	return std::stod( parameters_[name] );
+}
+
+bool Properties::getBoolValue(std::string name)
+{
+	return toBool( parameters_[name] );
+}
+
+}
---
+++ Properties.h
@@ -0,0 +1,43 @@
+/*
+ * Properties.h
+ *
+ *  Created on: 2013-05-06
+ *      Author: jarrett
+ */
+#ifndef PROPERTIES_H_
+#define PROPERTIES_H_
+
+#include <string.h>
+#include <map>
+
+#include <boost/config.hpp>
+#include <boost/program_options/detail/config_file.hpp>
+#include <boost/program_options/parsers.hpp>
+
+namespace pod = boost::program_options::detail;
+
+namespace utilities {
+	
+class Properties {
+public:
+	Properties(std::string filename);
+	virtual ~Properties();
+	
+	std::string getStringValue(std::string name);
+	int getIntValue(std::string name);
+	long getLongValue(std::string name);
+	float getFloatValue(std::string name);
+	double getDoubleValue(std::string name);
+	bool getBoolValue(std::string name);
+
+private:
+	std::string filename_;
+	std::map<std::string, std::string> parameters_;
+	
+	void initialize();
+	
+	bool toBool(std::string str);
+};
+
+}
+#endif /* PROPERTIES_H_ */
