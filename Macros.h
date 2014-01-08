b7a45a777b2b34c8a5344433c5dec347d9de0b5f
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-01-08 11:07:20 -0500

    - added more macros

--- Macros.h
+++ Macros.h
@@ -1,6 +1,25 @@
 #ifndef MACROS_H_
 #define MACROS_H_
 
+#define GET(Type, MemberName, FaceName) \
+	Type get##FaceName() const { \
+		return MemberName; \
+	};
+
+#define SET(Type, MemberName, FaceName) \
+	void set##FaceName(Type value) { \
+		MemberName = value; \
+	};
+
+#define SET_CONST_REF(Type, MemberName, FaceName) \
+	void set##FaceName(const Type& value) { \
+		MemberName = value; \
+	};
+
+#define GET_CONST_REF(Type, MemberName, FaceName) \
+	const Type& get##FaceName() const { \
+		return MemberName; \
+	};
 
 #define GETSET(Type, MemberName, FaceName) \
 	Type get##FaceName() const { \
@@ -10,12 +29,20 @@
 	void set##FaceName(Type value) { \
 		MemberName = value; \
 	};
-	
+
+#define GETSET_CONST_REF(Type, MemberName, FaceName) \
+	const Type& get##FaceName() const { \
+		return MemberName; \
+	}; \
+	\
+	void set##FaceName(const Type& value) { \
+		MemberName = value; \
+	};
+
 #define GETSET_INTERFACE(Type, FaceName) \
 	virtual Type get##FaceName() const = 0; \
 	\
 	virtual void set##FaceName(Type value) = 0;
-
 
 /*
 #define GETSET(ClassName, Type, MemberName, FaceName) \
