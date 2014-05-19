1c7d1d9805aa1d673757a7e8fb87a5a389983884
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-05-19 18:24:47 -0400

    - added function to determine if a vector is within a sphere (defined by a point and radius)

--- GlmUtilities.hpp
+++ GlmUtilities.hpp
@@ -11,6 +11,16 @@ const glm::vec3 GLM_UNIT_VECTOR_X = glm::vec3(1.0f, 0.0f, 0.0f);
 const glm::vec3 GLM_UNIT_VECTOR_Y = glm::vec3(0.0f, 1.0f, 0.0f);
 const glm::vec3 GLM_UNIT_VECTOR_Z = glm::vec3(0.0f, 0.0f, 1.0f);
 
+bool isVec3InSphere(const glm::vec3& point, const glm::vec3& sphereCenter, glm::detail::float32 radius)
+{
+	// sqrt((x-x0)^2 + (y-y0)^2 + (z-z0)^2) <=? r
+	// (x-x0)^2 + (y-y0)^2 + (z-z0)^2
+	
+	const glm::vec3 t = sphereCenter - point;
+	
+	return t.x*t.x + t.y*t.y + t.z*t.z <= radius*radius;
+}
+
 }
 
 #endif /* GLMUTILITIES_H_ */
