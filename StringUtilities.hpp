32b823eb77d56801e108d85885fc11931042231b
Jarrett Chisholm <j.chisholm@chisholmsoft.com>
2014-07-01 18:09:34 -0400

    - some fixes for multiple definition errors - refactoring for effeciency

--- StringUtilities.hpp
+++ StringUtilities.hpp
@@ -6,14 +6,20 @@
 namespace utilities
 {
 
-std::string toString(const std::wstring ws)
+// Make the functions in an anonymous namespace to avoid multiple definition errors at link time
+namespace
+{
+
+std::string toString(const std::wstring& ws)
 {
 	return std::string( ws.begin(), ws.end() );
 }
 
-std::wstring toWstring(const std::string s)
+std::wstring toWstring(const std::string& s)
 {
 	return std::wstring( s.begin(), s.end() );
+}
+
 }
 
 }
